de57958b92a6cf3242dcce533ae43c9659248b62
Aditya Dubey <dubeyadi9661@gmail.com>
2025-04-05 01:31:52 +0530

    Update App.js

--- SmartBridge_MERN_Assignment-main/week_3_assignment/src/App.js
+++ SmartBridge_MERN_Assignment-main/week_3_assignment/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="flex flex-col items-center gap-8 p-10">
       <TextUpdater />
       <SimpleForm />
-      <UserCard name="Aditya Dubey" email="Shashidhar.K@vit.com" />
+      <UserCard name="Aditya Dubey" email="adityadubey@gmail.com" />
       <Button />
       <LoginForm />
     </div>
